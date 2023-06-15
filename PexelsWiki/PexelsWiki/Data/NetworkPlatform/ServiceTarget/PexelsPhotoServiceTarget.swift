16fd89d33cba9d1344d14ab8da855a5602bc3179
yjjem <marksim0606@gmail.com>
2023-06-15 15:45:18 +0900

    feat: add PexelsPhotoServiceTarget Requestable

---
+++ PexelsWiki/PexelsWiki/Data/NetworkPlatform/ServiceTarget/PexelsPhotoServiceTarget.swift
@@ -0,0 +1,63 @@
+//
+//  PexelsPhotoServiceTarget.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+enum PexelsPhotoServiceTarget {
+    case searchPhotos(
+        query: String,
+        orientation: ContentOrientation,
+        size: ContentSize,
+        page: Int,
+        perPage: Int
+    )
+    case curatedPhotos(
+        page: Int,
+        perPage: Int
+    )
+}
+
+extension PexelsPhotoServiceTarget: Requestable {
+    typealias Response = Item
+    
+    var baseURL: URL { return URL(string: "https://api.pexels.com")! }
+    
+    var path: String {
+        switch self {
+        case .searchPhotos: return "/v1/search"
+        case .curatedPhotos: return "/v1/curated"
+        }
+    }
+    
+    var queries: [String: String] {
+        switch self {
+        case let .searchPhotos(query, orientation, size, page, perPage):
+            return [
+                "query": query,
+                "orientation": orientation.name,
+                "size": size.name,
+                "page": String(page),
+                "per_page": String(perPage)
+            ]
+        case let .curatedPhotos(page, perPage):
+            return [
+                "page": String(page),
+                "per_page": String(perPage)
+            ]
+        }
+    }
+    
+    var headers: [String: String] {
+        return [
+            "Authentication": "key"
+        ]
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+}
