f95dfb2d02dab1ef0d1dc5135222436e399f7ad8
yjjem <marksim0606@gmail.com>
2023-06-15 15:44:44 +0900

    feat: add Content Details

---
+++ PexelsWiki/PexelsWiki/Domain/Model/ContentDetails.swift
@@ -0,0 +1,36 @@
+//
+//  ContentDetails.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+enum ContentOrientation {
+    case landscape
+    case portrait
+    case square
+    
+    var name: String {
+        switch self {
+        case .landscape: return "landscape"
+        case .portrait: return "portrait"
+        case .square: return "square"
+        }
+    }
+}
+
+enum ContentSize {
+    case large
+    case medium
+    case small
+    
+    var name: String {
+        switch self {
+        case .large: return "large"
+        case .medium: return "medium"
+        case .small: return "small"
+        }
+    }
+}
