f4b6e395b93c3570ad1da205182bfaadc80a77c8
yjjem <marksim0606@gmail.com>
2023-06-15 15:47:20 +0900

    feat: implement PexelsVideoRepository

---
+++ PexelsWiki/PexelsWiki/Data/NetworkPlatform/Repository/PexelsVideoRepository.swift
@@ -0,0 +1,65 @@
+//
+//  PexelsVideoRepository.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+final class PexelsVideoRepository: PexelsVideoRepositoryInterface {
+    
+    private let provider: Networkable
+    
+    init(provider: Networkable) {
+        self.provider = provider
+    }
+    
+    func loadPopularVideos(
+        minWidth: Int,
+        minHeight: Int,
+        minDuration: Int,
+        maxDuration: Int,
+        page: Int,
+        perPage: Int,
+        _ completion: @escaping (Result<Item, RepositoryError>) -> Void
+    ) {
+        let target: PexelsVideoServiceTarget = .popularVideos(
+            minWidth: minWidth,
+            minHeight: minHeight,
+            minDuration: minDuration,
+            maxDuration: maxDuration,
+            page: page,
+            perPage: perPage
+        )
+        
+        provider.load(target) { response in
+            response.onComplete { data in
+                completion(.success(data))
+            }
+        }
+    }
+    
+    func searchVideos(
+        query: String,
+        orientation: ContentOrientation,
+        size: ContentSize,
+        page: Int,
+        perPage: Int,
+        _ completion: @escaping (Result<Item, RepositoryError>) -> Void
+    ) {
+        let target: PexelsVideoServiceTarget = .searchVideos(
+            query: query,
+            orientation: orientation,
+            size: size,
+            page: page,
+            perPage: perPage
+        )
+        
+        provider.load(target) { response in
+            response.onComplete { data in
+                completion(.success(data))
+            }
+        }
+    }
+}
