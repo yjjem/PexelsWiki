30b6f2c643da41bdc4f4f93a6bd185bdc4698733
yjjem <marksim0606@gmail.com>
2023-06-15 15:42:07 +0900

    feat: add Requestable

---
+++ PexelsWiki/PexelsWiki/Data/NetworkPlatform/NetworkProvider/Requestable.swift
@@ -0,0 +1,40 @@
+//
+//  Requestable.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+protocol Requestable {
+    associatedtype Response: Decodable
+    
+    var baseURL: URL { get }
+    var path: String { get }
+    var queries: [String: String] { get }
+    var headers: [String: String] { get }
+    var method: HTTPMethod { get }
+}
+
+extension Requestable {
+    
+    var url: URL {
+        var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        components?.path = path
+        components?.queryItems = queries.map { key, value in
+            URLQueryItem(name: key, value: value)
+        }
+        
+        guard let url = components?.url else {
+            fatalError()
+        }
+        
+        return url
+    }
+    
+    var urlRequest: URLRequest {
+        let urlRequest = URLRequest(url: url, method: method, headers: headers)
+        return urlRequest
+    }
+}
