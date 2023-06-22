ed4b950216b11f6dd12881c5a8529ae5cea0322d
yjjem <marksim0606@gmail.com>
2023-06-22 22:57:42 +0900

    feat: add PexelsPhotoUseCase

---
+++ PexelsWiki/PexelsWiki/Domain/UseCase/PexelsPhotoUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  CuratedPhotoUseCase.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+protocol PexelsPhotoUseCaseInterface {
+    
+    func loadCuratedPhotoPage(
+        page: Int,
+        perPage: Int,
+        completion: @escaping (Result<PhotoPage, Error>) -> Void
+    )
+}
+
+final class PexelsPhotoUseCase: PexelsPhotoUseCaseInterface {
+    
+    private let repository: PexelsPhotoRepositoryInterface
+    
+    init(repository: PexelsPhotoRepositoryInterface) {
+        self.repository = repository
+    }
+    
+    func loadCuratedPhotoPage(
+        page: Int,
+        perPage: Int,
+        completion: @escaping (Result<PhotoPage, Error>) -> Void
+    ) {
+        repository.loadCuratedPhotos(page: page, perPage: perPage) { response in
+            response.onComplete { photoPage in
+                completion(.success(photoPage))
+            }
+        }
+    }
+}
