53cbdd4c25ee8ffe4ea41412c90c067098cb2d47
yjjem <marksim0606@gmail.com>
2023-06-24 13:39:13 +0900

    feat: add PexelsVideoSearchUseCase

---
+++ PexelsWiki/PexelsWiki/Domain/UseCase/PexelsVideoSearchUseCase.swift
@@ -0,0 +1,50 @@
+//
+//  PexelsVideoSearchUseCase.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+import Foundation
+
+protocol PexelsVideoSearchUseCaseInterface {
+    
+    func search(
+        query: String,
+        orientation: ContentOrientation,
+        size: ContentSize,
+        page: Int,
+        perPage: Int,
+        _ completion: @escaping (Result<VideoPage, Error>) -> Void
+    )
+}
+
+final class PexelsVideoSearchUseCase: PexelsVideoSearchUseCaseInterface {
+    
+    private let repository: PexelsVideoRepositoryInterface
+    
+    init(repository: PexelsVideoRepositoryInterface) {
+        self.repository = repository
+    }
+    
+    func search(
+        query: String,
+        orientation: ContentOrientation,
+        size: ContentSize,
+        page: Int,
+        perPage: Int,
+        _ completion: @escaping (Result<VideoPage, Error>) -> Void
+    ) {
+        repository.searchVideos(
+            query: query,
+            orientation: orientation,
+            size: size,
+            page: page,
+            perPage: perPage
+        ) { response in
+            response.onComplete { videoPage in
+                completion(.success(videoPage))
+            }
+        }
+    }
+}
