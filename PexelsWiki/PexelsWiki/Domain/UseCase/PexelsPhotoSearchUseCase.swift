e41274d74c3301dfe2f5c310e6a73d4cb8092f2f
yjjem <marksim0606@gmail.com>
2023-06-24 13:39:25 +0900

    feat: add PexelsPhotoSearchUseCase

---
+++ PexelsWiki/PexelsWiki/Domain/UseCase/PexelsPhotoSearchUseCase.swift
@@ -0,0 +1,51 @@
+//
+//  PexelsPhotoSearchUseCase.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+import Foundation
+
+protocol PexelsPhotoSearchUseCaseInterface {
+    
+    func search(
+        query: String,
+        orientation: ContentOrientation,
+        size: ContentSize,
+        page: Int,
+        perPage: Int,
+        _ completion: @escaping (Result<PhotoPage, Error>) -> Void
+    )
+}
+
+final class PexelsPhotoSearchUseCase: PexelsPhotoSearchUseCaseInterface {
+    
+    private let repository: PexelsPhotoRepositoryInterface
+    
+    init(repository: PexelsPhotoRepositoryInterface) {
+        self.repository = repository
+    }
+    
+    func search(
+        query: String,
+        orientation: ContentOrientation,
+        size: ContentSize,
+        page: Int,
+        perPage: Int,
+        _ completion: @escaping (Result<PhotoPage, Error>) -> Void
+    ) {
+        repository.searchPhotos(
+            query: query,
+            orientation: orientation,
+            size: size,
+            page: page,
+            perPage: perPage
+        ) { response in
+            
+            response.onComplete { photoPage in
+                completion(.success(photoPage))
+            }
+        }
+    }
+}
