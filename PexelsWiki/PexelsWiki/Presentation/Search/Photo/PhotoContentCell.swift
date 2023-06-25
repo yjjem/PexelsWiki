71271d8cfe1284a796a5134bc945d2d9fb400d86
yjjem <marksim0606@gmail.com>
2023-06-25 14:50:46 +0900

    feat: add PhotoContentCell

---
+++ PexelsWiki/PexelsWiki/Presentation/Search/Photo/PhotoContentCell.swift
@@ -0,0 +1,54 @@
+//
+//  ImageSearchCell.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+import UIKit
+
+final class PhotoContentCell: UICollectionViewCell {
+    
+    private let imageView: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    
+    private let userInfoView: UserInfoView = {
+        let userInfo = UserInfoView()
+        return userInfo
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(using: PhotoContentCellViewModel) {
+        // TODO: Implement configuration
+    }
+    
+    private func configureLayoutConstraints() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(userInfoView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.9)
+        ])
+        
+        userInfoView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            userInfoView.topAnchor.constraint(equalTo: imageView.topAnchor),
+            userInfoView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            userInfoView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            userInfoView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
