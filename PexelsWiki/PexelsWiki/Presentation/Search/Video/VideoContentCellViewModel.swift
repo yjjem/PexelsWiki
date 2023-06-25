2d325630641d55d0306c7f870d22000f9085df92
yjjem <marksim0606@gmail.com>
2023-06-25 19:46:41 +0900

    feat: add VideoContentCellViewModel

---
+++ PexelsWiki/PexelsWiki/Presentation/Search/Video/VideoContentCellViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  VideoContentCellViewModel.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+struct VideoContentCellViewModel {
+    let videoURLString: String
+    let userName: String
+}
