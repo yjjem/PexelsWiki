2e74022cd0e57e83003f7a8a99da17e4e6efaa96
yjjem <marksim0606@gmail.com>
2023-06-25 14:50:34 +0900

    feat: add PhotoContentCellViewModel

---
+++ PexelsWiki/PexelsWiki/Presentation/Search/Photo/PhotoContentCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoContentViewModel.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+struct PhotoContentCellViewModel {
+    let imageURLString: String
+    let userName: String
+}
