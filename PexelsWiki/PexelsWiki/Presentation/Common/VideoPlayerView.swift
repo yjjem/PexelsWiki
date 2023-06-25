3e0c3bfb91b662d003822b8ab225b20777429b36
yjjem <marksim0606@gmail.com>
2023-06-25 18:12:40 +0900

    feat: add VideoPlayerView

---
+++ PexelsWiki/PexelsWiki/Presentation/Common/VideoPlayerView.swift
@@ -0,0 +1,45 @@
+//
+//  VideoPlayerView.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import UIKit
+import AVKit
+
+final class VideoPlayerView: UIView, VideoPlayable {
+    
+    enum PlayerState {
+        case active
+        case inactive
+    }
+    
+    var playerState: PlayerState = .inactive {
+        didSet {
+            stateDidChange()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configurePlayerLayer()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func stateDidChange() {
+        switch playerState {
+        case .active: playVideo()
+        case .inactive: pauseVideo()
+        }
+    }
+    
+    func configurePlayerLayer() {
+        guard let playerLayer else { return }
+        layer.addSublayer(playerLayer)
+    }
+}
