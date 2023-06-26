09c8a1edfad26196f3d4fc886be3e09a1c03afda
yjjem <marksim0606@gmail.com>
2023-06-26 22:14:20 +0900

    feat: add SearchFilterViewModel

---
+++ PexelsWiki/PexelsWiki/Presentation/Search/Filter/SearchFilterViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchFilterViewModel.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+final class SearchFilterViewModel {
+    var selectedOrientation: ContentOrientation = .landscape
+    var selectedSize: ContentSize = .large
+}
