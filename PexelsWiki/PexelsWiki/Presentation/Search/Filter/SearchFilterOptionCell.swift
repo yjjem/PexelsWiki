b73a04a767961a149a37ab27cfbea4b696137861
yjjem <marksim0606@gmail.com>
2023-06-26 22:14:40 +0900

    feat: add SearchFilterOptionCell

---
+++ PexelsWiki/PexelsWiki/Presentation/Search/Filter/SearchFilterOptionCell.swift
@@ -0,0 +1,21 @@
+//
+//  SearchFilterOptionCell.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+import UIKit
+
+final class SearchFilterOptionCell: UITableViewCell {
+    static let reuseIdentifier: String = #file
+    
+    func addCheckMark() {
+        accessoryType = .checkmark
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        accessoryType = selected ? .checkmark : .none
+    }
+}
