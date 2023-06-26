574de792df88c3ac62b907a287f762f11aa7c816
yjjem <marksim0606@gmail.com>
2023-06-26 22:16:57 +0900

    feat: add FilterOptions

---
+++ PexelsWiki/PexelsWiki/Presentation/Search/Filter/FilterOptions.swift
@@ -0,0 +1,11 @@
+//
+//  FilterOptions.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+struct FilterOptions {
+    let orientation: ContentOrientation
+    let size: ContentSize
+}
