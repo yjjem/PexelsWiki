54693e7e526d845f1752785eba238f8f01903d81
yjjem <marksim0606@gmail.com>
2023-06-27 17:44:32 +0900

    feat: add ImageLoadManager with cache

---
+++ PexelsWiki/PexelsWiki/Utilities/ImageLoadManager.swift
@@ -0,0 +1,77 @@
+//
+//  MediaLoadManager.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+
+
+import Foundation
+
+protocol Cancellable {
+    func cancel()
+}
+
+extension URLSessionDataTask: Cancellable { }
+
+final class ImageLoadManager {
+    
+    private static let imageCache = URLCache(
+        memoryCapacity: 200.mb,
+        diskCapacity: 500.mb
+    )
+    
+    static func fetchCachedImageDataElseLoad(
+        url: URL,
+        _ completion: @escaping (Result<Data, Error>) -> Void
+    ) -> URLSessionDataTask? {
+        let request = URLRequest(url: url)
+        
+        if let cached = imageCache.cachedResponse(for: request) {
+            completion(.success(cached.data))
+            return nil
+        } else {
+            return loadImageData(request: request, completion)
+        }
+    }
+    
+    private static func loadImageData(
+        request: URLRequest,
+        _ completion: @escaping (Result<Data, Error>) -> Void
+    ) -> URLSessionDataTask {
+        
+        let uiQueue = DispatchQueue.main
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let error {
+                uiQueue.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            if let data, let response {
+                
+                if imageCache.cachedResponse(for: request) == nil {
+                    let cached = CachedURLResponse(response: response, data: data)
+                    imageCache.storeCachedResponse(cached, for: request)
+                }
+                
+                uiQueue.async {
+                    completion(.success(data))
+                }
+            }
+        }
+        
+        task.resume()
+        
+        return task
+    }
+}
+
+fileprivate extension Int {
+    
+    var mb: Int {
+        return self * 1024 * 1024
+    }
+}
