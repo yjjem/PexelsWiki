8c9a500bb70db9a4d691d5ed056fb6417ca3aad5
yjjem <marksim0606@gmail.com>
2023-08-07 02:53:58 +0900

    refactor(SearchFilterViewController): viewDidLoad 및 주석 제거

--- PexelsWiki/PexelsWiki/Presentation/Search/Filter/SearchFilterViewController.swift
+++ PexelsWiki/PexelsWiki/Presentation/Search/Filter/SearchFilterViewController.swift
@@ -32,12 +32,6 @@ final class SearchFilterViewController: UIViewController {
         configureSearchFilterTableView()
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        preselectOptions()
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
