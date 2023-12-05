db0d838b50447a837ea8b918622686c662fcde6b
yjjem <marksim0606@gmail.com>
2023-12-05 17:32:08 +0900

    feat: PaginationFetchControl 추가

---
+++ PexelsWiki/PexelsWiki/Presentation/Common/PaginationFetchControl.swift
@@ -0,0 +1,34 @@
+//
+//  ScrollPagingControl.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import UIKit
+
+final class PaginationFetchControl: NSObject, UIScrollViewDelegate {
+    
+    // MARK: Property(s)
+    
+    var didTriggerFetchMore: (() -> Void)?
+    
+    private var scrollView: UIScrollView?
+    
+    // MARK: Function(s)
+    
+    func configure(scrollView: UIScrollView) {
+        self.scrollView = scrollView
+        scrollView.delegate = self
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let visibleBottomPointY = scrollView.bounds.minY
+        let paginationTriggerPointY = scrollView.contentSize.height * 0.8
+        let didReachPaginationTriggerPoint = visibleBottomPointY >= paginationTriggerPointY
+        
+        if didReachPaginationTriggerPoint {
+            didTriggerFetchMore?()
+        }
+    }
+}
