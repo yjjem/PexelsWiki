7459ad88797e4441e02428883a9e61e4076b1f7b
yjjem <marksim0606@gmail.com>
2024-02-13 16:30:59 +0900

    refactor: PhotoContentCell 레이블 삭제 및 컨스트레인 수정

--- PexelsWiki/PexelsWiki/Presentation/Search/Photo/PhotoContentCell.swift
+++ PexelsWiki/PexelsWiki/Presentation/Search/Photo/PhotoContentCell.swift
@@ -9,24 +9,16 @@ import UIKit
 
 final class PhotoContentCell: UICollectionViewCell {
     
-    // MARK: Variable(s)
+    // MARK: Property(s)
     
-    private let imageView: UIImageView = {
-        let image = UIImageView()
-        return image
-    }()
-    
-    private let userInfoView: UserInfoView = {
-        let userInfo = UserInfoView()
-        return userInfo
-    }()
-    
+    private let photoThumbnailView: UIImageView = UIImageView()
     private var contentLoad: Cancellable?
     
     // MARK: Override(s)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureHierarchy()
         configureLayoutConstraints()
     }
     
@@ -34,14 +26,8 @@ final class PhotoContentCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    deinit {
-        contentLoad?.cancel()
-    }
-    
     override func prepareForReuse() {
         super.prepareForReuse()
-        
-        imageView.image = nil
         contentLoad?.cancel()
     }
     
@@ -49,39 +35,31 @@ final class PhotoContentCell: UICollectionViewCell {
     
     func configure(using viewModel: PhotoContentCellViewModel) {
         
-        userInfoView.setupUserName(using: viewModel.userName)
-        
         // TODO: 셀이 로딩 로직을 모르도록 수정할 것
-        
+        print(viewModel)
         contentLoad = ImageLoadManager.fetchCachedImageDataElseLoad(
             urlString: viewModel.imageURLString
         ) { [weak self] response in
             
             guard let self else { return }
-            _ = response.map(self.imageView.addImage)
+            _ = response.map(self.photoThumbnailView.addImage)
         }
     }
     
     // MARK: Private Function(s)
     
+    private func configureHierarchy() {
+        contentView.addSubview(photoThumbnailView)
+    }
+    
     private func configureLayoutConstraints() {
-        contentView.addSubview(imageView)
-        contentView.addSubview(userInfoView)
-        
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        photoThumbnailView.contentMode = .scaleAspectFit
+        photoThumbnailView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.9)
-        ])
-        
-        userInfoView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            userInfoView.topAnchor.constraint(equalTo: imageView.bottomAnchor),
-            userInfoView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            userInfoView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            userInfoView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            photoThumbnailView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            photoThumbnailView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            photoThumbnailView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            photoThumbnailView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     }
 }
