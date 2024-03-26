cf8f10ca1b2c6a927c56859882e20efe8e0531b4
yjjem <marksim0606@gmail.com>
2024-03-27 07:29:39 +0900

    feat: PhotoListViewModel 이 이미지 크기를 CGSize로 변환 및 downscale 추가

--- PexelsWiki/PexelsWiki/Presentation/Scene/PhotoList/ViewModel/PhotoContentCellViewModel.swift
+++ PexelsWiki/PexelsWiki/Presentation/Scene/PhotoList/ViewModel/PhotoContentCellViewModel.swift
@@ -11,4 +11,15 @@ struct PhotoContentCellViewModel: Hashable {
     let userName: String
     let imageURLString: String
     let imageID: Int
+    let imageWidth: Int
+    let imageHeight: Int
+    
+    func imageSize() -> CGSize {
+        return CGSize(width: imageWidth, height: imageHeight)
+    }
+    
+    func downScaledImageSize(by downscaleValue: CGFloat) -> CGSize{
+        let downScale = CGAffineTransform(scaleX: downscaleValue, y: downscaleValue)
+        return imageSize().applying(downScale)
+    }
 }
