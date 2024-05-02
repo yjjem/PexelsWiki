be213f8a3dc3c063d6e0cb8ee61ee6dcd65692be
yjjem <marksim0606@gmail.com>
2024-05-02 23:45:12 +0900

    refactor: PhotoContentCellViewModel 사용하지 않는 메서드 제거

--- PexelsWiki/PexelsWiki/Presentation/Scene/PhotoList/ViewModel/PhotoContentCellViewModel.swift
+++ PexelsWiki/PexelsWiki/Presentation/Scene/PhotoList/ViewModel/PhotoContentCellViewModel.swift
@@ -13,13 +13,4 @@ struct PhotoContentCellViewModel: Hashable {
     let imageID: Int
     let imageWidth: Int
     let imageHeight: Int
-    
-    func imageSize() -> CGSize {
-        return CGSize(width: imageWidth, height: imageHeight)
-    }
-    
-    func downScaledImageSize(by downscaleValue: CGFloat) -> CGSize{
-        let downScale = CGAffineTransform(scaleX: downscaleValue, y: downscaleValue)
-        return imageSize().applying(downScale)
-    }
 }
