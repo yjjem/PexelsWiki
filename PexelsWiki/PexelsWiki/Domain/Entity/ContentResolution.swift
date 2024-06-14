43077ff8a3658e43c6213a7652ac6c5383be61b7
yjjem <marksim0606@gmail.com>
2024-06-14 15:04:32 +0900

    fix: ContentResolution 엔티티에 UI관련 메서드가 있던 점 수정

--- PexelsWiki/PexelsWiki/Domain/Entity/ContentResolution.swift
+++ PexelsWiki/PexelsWiki/Domain/Entity/ContentResolution.swift
@@ -8,12 +8,4 @@
 struct ContentResolution {
     let width: Int
     let height: Int
-    
-    func toString() -> String {
-        return String(width) + " X " + String(height)
-    }
-    
-    func pixelsCount() -> Int {
-        return width * height
-    }
 }
