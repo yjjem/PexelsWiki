2193791ff3deb78ebccaa2c525af755e64131afe
yjjem <marksim0606@gmail.com>
2024-07-03 20:07:28 +0900

    feat: SearchPhotosUseCaseError 구현

---
+++ PexelsWiki/PexelsWiki/Domain/UseCase/SearchPhotosUseCaseError.swift
@@ -0,0 +1,11 @@
+//
+//  SearchPhotosUseCaseError.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2024 Jeremy All rights reserved.
+    
+
+enum SearchPhotosUseCaseError: Error {
+    case searchFailed
+    case endOfPage
+}
