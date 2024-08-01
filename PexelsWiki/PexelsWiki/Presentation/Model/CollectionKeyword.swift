0afb636f18ddd487f4ef1bc1c8a8ad264ef23659
yjjem <marksim0606@gmail.com>
2024-08-02 00:49:12 +0900

    feat: CollectionKeywords Presentation 모델 추가

---
+++ PexelsWiki/PexelsWiki/Presentation/Model/CollectionKeyword.swift
@@ -0,0 +1,11 @@
+//
+//  CollectionKeyword.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2024 Jeremy All rights reserved.
+
+
+struct CollectionKeyword {
+    let title: String
+    let totalItems: Int
+}
