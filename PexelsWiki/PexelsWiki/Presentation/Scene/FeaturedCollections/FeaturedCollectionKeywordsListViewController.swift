ec77c330eb99c68b25e302f548c33dbd9967f159
yjjem <marksim0606@gmail.com>
2024-08-02 16:03:09 +0900

    feat: FeaturedCollectionKeywordsListViewController 파일 생성

---
+++ PexelsWiki/PexelsWiki/Presentation/Scene/FeaturedCollections/FeaturedCollectionKeywordsListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FeaturedCollectionViewController.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2024 Jeremy All rights reserved.
+
+
+import UIKit
+
+final class FeaturedCollectionKeywordsListViewController: UIViewController {
+    
+}
