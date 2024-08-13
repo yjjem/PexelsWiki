059d0a559400efc3cdf0557d0c59068bf9defddd
yjjem <marksim0606@gmail.com>
2024-08-14 01:36:24 +0900

    feat: SearchNavigatorViewModel에 FeaturedCollectionsUseCase 기능 추가

--- PexelsWiki/PexelsWiki/Presentation/Scene/SearchNavigator/ViewModel/SearchNavigatorViewModel.swift
+++ PexelsWiki/PexelsWiki/Presentation/Scene/SearchNavigator/ViewModel/SearchNavigatorViewModel.swift
@@ -4,12 +4,23 @@
 //
 //  Copyright (c) 2023 Jeremy All rights reserved.
 
+import Foundation
+
 
 final class SearchNavigatorViewModel {
     
     // MARK: Property(s)
     
+    var loadedFeaturedCollectionKeywords: (([FeaturedCollectionCellViewModel]) -> Void)?
+    var cancelToken: Cancellable?
+    
     private var query: String = ""
+    
+    private let useCase: DiscoverFeaturedCollectionsUseCase
+    
+    init(useCase: DiscoverFeaturedCollectionsUseCase) {
+        self.useCase = useCase
+    }
     
     // MARK: Function(s)
     
@@ -23,5 +34,22 @@ final class SearchNavigatorViewModel {
     
     func shuffledCategories() -> [RecommendedCategoryCellViewModel] {
         return RecommendedCategoryCellViewModel.allCases.shuffled()
+    }
+    
+    func onViewDidLoad() {
+        let command = DiscoverFeaturedCollectionsCommand(requiresRefresh: false)
+        cancelToken = useCase.discoverFeaturedCollections(command) { [weak self] response in
+            guard case .success(let featuredCollection) = response else {
+                return
+            }
+            
+            let mappedViewModels = featuredCollection.collections.map {
+                FeaturedCollectionCellViewModel(title: $0.title, description: $0.description,totalItems: $0.mediaCount)
+            }
+            
+            DispatchQueue.main.async {
+                self?.loadedFeaturedCollectionKeywords?(mappedViewModels)
+            }
+        }
     }
 }
