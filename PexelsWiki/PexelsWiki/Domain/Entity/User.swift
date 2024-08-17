6ef26a6b6d6f80027d224ae822d05ea2e8af6367
yjjem <marksim0606@gmail.com>
2024-08-18 02:53:20 +0900

    refactor:  User Decodable 채택하도록 변경

--- PexelsWiki/PexelsWiki/Domain/Entity/User.swift
+++ PexelsWiki/PexelsWiki/Domain/Entity/User.swift
@@ -5,8 +5,14 @@
 //  Copyright (c) 2024 Jeremy All rights reserved.
 
 
-struct User {
+struct User: Decodable {
     let id: Int
     let name: String
     let profileURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case profileURL = "url"
+    }
 }
