572a8502a274e42e1e58b0d4be1732b894031441
yjjem <marksim0606@gmail.com>
2024-08-19 15:23:51 +0900

    feat: FetchCollectionMediaUseCase 추가

---
+++ PexelsWiki/PexelsWiki/Domain/UseCase/FetchCollectionMedia/FetchCollectionMediaUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  FetchCollectionMediaUseCase.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2024 Jeremy All rights reserved.
+    
+
+protocol FetchCollectionMediaUseCase {
+    func fetchCollectionMedia(
+        _ targetCollectionIdentifier: String,
+        _ completion: @escaping (Result<CollectionMedia, Error>) -> Void
+    ) -> Cancellable?
+}
