3a73202a76bd27c0b2e2ca63f30edacacb38bab6
yjjem <marksim0606@gmail.com>
2024-08-19 15:36:40 +0900

    feat: FeatchCollectionMediaPort 추가

---
+++ PexelsWiki/PexelsWiki/Domain/Ports/FetchCollectionMediaPort.swift
@@ -0,0 +1,13 @@
+//
+//  FetchCollectionMediaPort.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2024 Jeremy All rights reserved.
+    
+
+protocol FetchCollectionMediaPort {
+    func fetchCollectionMedia(
+        _ targetCollectionIdentifier: String,
+        _ completion: @escaping (Result<CollectionMedia, CollectionMediaUseCaseError>) -> Void
+    ) -> Cancellable?
+}
