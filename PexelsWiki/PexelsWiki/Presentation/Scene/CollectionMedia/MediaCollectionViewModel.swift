1e1ba3cc668aafa8f2f3eed8fb7455a97a7d9aaa
yjjem <marksim0606@gmail.com>
2024-08-29 00:36:22 +0900

    refactor: MediaCollectionViewModel 도메인 객체 대신 뷰에 맞는 프리뷰 객체 생성하여 리팩토링

--- PexelsWiki/PexelsWiki/Presentation/Scene/CollectionMedia/MediaCollectionViewModel.swift
+++ PexelsWiki/PexelsWiki/Presentation/Scene/CollectionMedia/MediaCollectionViewModel.swift
@@ -7,18 +7,29 @@
 
 final class MediaCollectionViewModel {
     
+    // MARK: Type(s)
+    
+    struct MediaPreview: Hashable {
+        let image: String
+        let user: String
+        let identifier: String
+        
+        init(image: String, user: String, identifier: Int) {
+            self.image = image
+            self.user = user
+            self.identifier = String(identifier)
+        }
+    }
+    
     // MARK: Property(s)
     
-    var loadedMediaCollection: ((CollectionMedia) -> Void)?
+    var loadedMediaPreviews: (([MediaPreview]) -> Void)?
     var cancelToken: Cancellable?
     
     private let useCase: RetrieveCollectionMediaService
     private let collectionIdentifier: String
     
-    init(
-        useCase: RetrieveCollectionMediaService,
-        collectionIdentifier: String
-    ) {
+    init(useCase: RetrieveCollectionMediaService, collectionIdentifier: String) {
         self.useCase = useCase
         self.collectionIdentifier = collectionIdentifier
     }
@@ -30,16 +41,32 @@ final class MediaCollectionViewModel {
     // MARK: Function(s)
     
     func onViewDidLoad() {
-        cancelToken = useCase.fetchCollectionMedia(collectionIdentifier) {
-            [weak self] response in
-            
-            // TODO: Handle Error
-            
+        cancelToken = useCase.fetchCollectionMedia(collectionIdentifier) { [weak self] response in
             guard case .success(let collectionMedia) = response else {
                 return
             }
             
-            self?.loadedMediaCollection?(collectionMedia)
+            let previewItems = collectionMedia.media.map { media in
+                
+                let imageURL: String
+                let userName: String
+                let identifier: Int
+                
+                switch media {
+                case .photo(let photoCollectionMedia):
+                    imageURL = photoCollectionMedia.source.medium
+                    userName = photoCollectionMedia.photographer
+                    identifier = photoCollectionMedia.id
+                case .video(let videoCollectionMedia):
+                    imageURL = videoCollectionMedia.image
+                    userName = videoCollectionMedia.user.name
+                    identifier = videoCollectionMedia.user.id
+                }
+
+                return MediaPreview(image: imageURL, user: userName, identifier: identifier)
+            }
+            
+            self?.loadedMediaPreviews?(previewItems)
         }
     }
 }
