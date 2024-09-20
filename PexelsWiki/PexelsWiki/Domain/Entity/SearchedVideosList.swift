5cd51ec1e33bdc6a83077f6d71faee9c86d0f330
yjjem <marksim0606@gmail.com>
2024-09-21 02:45:28 +0900

    feat(Domain): SearchedVideoList 추가

---
+++ PexelsWiki/PexelsWiki/Domain/Entity/SearchedVideosList.swift
@@ -0,0 +1,11 @@
+//
+//  SearchedVideos.swift
+//  PexelsWiki
+//
+//  Copyright (c) 2024 Jeremy All rights reserved.
+    
+
+struct SearchedVideosList {
+    let videos: VideoList
+    let totalResultsFound: Int
+}
